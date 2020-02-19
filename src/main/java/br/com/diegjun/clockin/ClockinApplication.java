66adb295bbd06c7782be88829501bcf4c2bb3c02
diego <diego.junior10@gmail.com>
2020-02-18 22:03:59 -0300

    Estrutura inicial do projeto.

---
+++ src/main/java/br/com/diegjun/clockin/ClockinApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegjun.clockin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diegjun/clockin/ClockinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegjun.clockin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
